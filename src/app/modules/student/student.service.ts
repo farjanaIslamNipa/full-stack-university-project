aebeb16c23f23c39936819c8e89178d21d20a4f9
farjanaIslamNipa <nipa.farzana87@gmail.com>
2023-12-06 16:05:32 +0600

    worked on raw searching

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -6,8 +6,20 @@ import { User } from '../users/user.model';
 import { TStudent } from './student.interface';
 
 
-const getAllStudentsFromDB = async () => {
-  const result = await Student.find()
+const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+
+  let searchTerm = '';
+
+  if(query?.searchTerm){
+    searchTerm = query.searchTerm as string;
+  }
+  const result = await Student.find({
+    $or: ['email', 'name.firstName', 'presentAddress'].map(
+      (field) => ({
+        [field]: {$regex: searchTerm, $options: 'i'}
+      })
+    )
+  })
                               .populate('admissionSemester')
                               .populate({
                                   path: 'academicDepartment',
