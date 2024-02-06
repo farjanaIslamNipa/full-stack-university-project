abfdaa32a69039862c3ff2f2061903cb6684062f
farjanaIslamNipa <nipa.farzana87@gmail.com>
2024-02-06 13:34:06 +0600

    added bg color to the event module

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -17,7 +17,7 @@ const loginUser = catchAsync(async(req, res) => {
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'User logged in successfully',
+    message: 'User login successful',
     data: {
       accessToken,
       needsPasswordChange
